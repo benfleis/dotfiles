c7cba003c150ed1437c8b0ab7cc1858c9e0ef5e4
ben fleis <ben.fleis@gmail.com>
2015-06-01 09:11:35 +0200

    update lein profiles to use current nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-midje "3.1.3"]
                   [refactor-nrepl "1.0.5"]]
-        :dependencies [#_[org.clojure/tools.nrepl "0.2.10" :exclusions [[org.clojure/clojure]]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10" :exclusions [[org.clojure/clojure]]]
                        [io.aviso/pretty "0.1.17"]]
         :jvm-opts ["-Dapple.awt.UIElement=true"
                    #_"-Djava.awt.headless=true"
@@ -14,4 +14,5 @@
                 :timeout 120000
 
                 ;; https://portal.aviso.io/#/document/open-source/pretty/Current/repl.html
+                ;; pretty print exceptions in the repl
                 :nrepl-middleware [io.aviso.nrepl/pretty-middleware]}}
