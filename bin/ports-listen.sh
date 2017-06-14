12151997ef6e960dfc62fca47f055ec7b1f2e721
ben fleis <benfleis@uber.com>
2017-06-14 07:58:53 +0200

    Add colors, command args to ports-listen

--- bin/ports-listen.sh
+++ bin/ports-listen.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# lsof -nP -i4tcp -s tcp:listen | sort -k1b,9
-lsof -nP -i 4tcp -s tcp:listen -F pcfn | while read -r line || [[ -n $line ]]; do
-    type=${line::1}
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+function blue   { printf "${BLUE}$@${NC}"; }
+function yellow { printf "${YELLOW}$@${NC}"; }
+
+lsof -nP -i tcp -s tcp:listen -F pcnf | while read -r line || [[ -n $line ]]; do
+    type=${line:0:1}
     datum=${line:1}
     case $type in
-        p) pid=$datum ;; # pid ALWAYS comes first, afaict
-        c) printf "$datum [$pid]:\n" ;;
-        f) ;; # NOOP; always included
+        p)  # pid ALWAYS comes first, afaict
+            pid=$datum
+            args=$(ps -q $pid -o args= | cut -b 1-40)
+            ;;
+        c) printf "$(yellow "$datum") -- $pid -- $(blue "$args"):\n" ;;
         n) printf "    $datum\n"; nl=1 ;;
+        f) ;; # always emitted, ignore it
         *) echo "UNKNOWN COMMAND: $type" >&2
     esac
 done
