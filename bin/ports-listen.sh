7fafdd27b9e4b92224f9d46030dcc02d464e64fd
ben fleis <benfleis@uber.com>
2017-06-14 08:39:57 +0200

    Do tty check on colors

--- bin/ports-listen.sh
+++ bin/ports-listen.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m'
+set -eu pipefail
+
+if [ -t 1 ]; then
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[1;33m'
+    BLUE='\033[0;34m'
+    NC='\033[0m'
+fi
 
 function blue   { printf "${BLUE}$@${NC}"; }
 function yellow { printf "${YELLOW}$@${NC}"; }
@@ -17,8 +21,8 @@ lsof -nP -i tcp -s tcp:listen -F pcnf | while read -r line || [[ -n $line ]]; do
             pid=$datum
             args=$(ps -q $pid -o args= | cut -b 1-40)
             ;;
-        c) printf "$(yellow "$datum") -- $pid -- $(blue "$args"):\n" ;;
-        n) printf "    $datum\n"; nl=1 ;;
+        c) printf "$(yellow "$datum") -- $pid -- $(blue "$args")\n" ;;
+        n) printf "    $datum\n" ;;
         f) ;; # always emitted, ignore it
         *) echo "UNKNOWN COMMAND: $type" >&2
     esac
