d9285cf66269f627ad4628386dc84b177e8668d7
ben fleis <benfleis@users.noreply.github.com>
2025-08-09 23:58:01 -0400

    fix homebrew paths

--- .config/zsh/by-name/05-homebrew.zsh
+++ .config/zsh/by-name/05-homebrew.zsh
@@ -1,13 +1,11 @@
-command -v brew >/dev/null && {
+_PREFIX=/opt/homebrew
+[[ -x "$_PREFIX/bin/brew" ]] && {
   # found elsewhere, many opts to remove updates
   export HOMEBREW_NO_ANALYTICS=1
   # export HOMEBREW_NO_AUTO_UPDATE=1
   # export HOMEBREW_NO_INSTALL_UPGRADE=1
   # export HOMEBREW_NO_INSTALL_CLEANUP=1
   # export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
-  _PREFIX=$(brew --prefix)
-  export PATH="$_PREFIX/bin:$PATH"
+  export PATH="$_PREFIX/bin:$_PREFIX/sbin:$PATH"
   fpath=( "$_PREFIX/share/zsh/site-functions" $fpath )
 }
-
-
